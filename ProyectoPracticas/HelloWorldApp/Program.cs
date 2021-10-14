f7510858a4a65f550c7c0697358120bf490349d1
Ismael Illán García <iillgar@upv.edu.es>
2021-10-14 11:00:23 +0200

    Pract1 hecha, Isma y Felix ;)

---
+++ ProyectoPracticas/HelloWorldApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        private static List<string> nacionalities;
+        private static void InitList()
+        {
+            nacionalities = new List<string>()
+            {
+                "Australian",
+                "Mongolian",
+                "Russian",
+                "Austrian",
+                "Brazilian"
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            InitList();
+            nacionalities.Sort();
+            foreach (string nacionality in nacionalities)
+            {
+                Console.WriteLine(nacionality);
+            }
+        }
+    }
+}
