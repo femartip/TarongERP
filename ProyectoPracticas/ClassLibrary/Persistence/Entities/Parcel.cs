4e5addf5aff41e658fd16cc34840b3acdc7b0d84
Ismael Illán García <iillgar@upv.edu.es>
2021-10-19 09:00:09 +0200

    clases to wapas

--- ProyectoPracticas/ClassLibrary/Persistence/Entities/Parcel.cs
+++ ProyectoPracticas/ClassLibrary/Persistence/Entities/Parcel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary.Persistence.Entities
+namespace TarongISW.Persistence.Entities
 {
-    class Class1
+    public partial class Parcel
     {
     }
 }
---
+++ ProyectoPracticas/ClassLibrary/Persistence/Entities/Temporary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarongISW.Persistence.Entities
+{
+    public partial class Temporary
+    {
+    }
+}
