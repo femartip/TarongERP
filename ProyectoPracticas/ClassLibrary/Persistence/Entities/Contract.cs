42542933331a9bb47dc6988c2a27ea134a25a1ab
Sergio Arnau Gómez <sarngom@upv.edu.es>
2021-10-19 09:16:54 +0200

    Clase Contract

--- ProyectoPracticas/ClassLibrary/Persistence/Entities/Contract.cs
+++ ProyectoPracticas/ClassLibrary/Persistence/Entities/Contract.cs
@@ -8,5 +8,41 @@ namespace TarongISW.Persistence.Entities
 {
     public partial class Contract
     {
+       
+        public string BankAccount
+        {
+            get;
+            set;
+        }
+        public int Id
+        {
+            get;
+            set;
+        }
+        public DateTime InitialDate
+        {
+            get;
+            set;
+        }
+        public string SSN
+        {
+            get;
+            set;
+        }
+        public virtual Person Hired
+        {
+            get;
+            set;
+        }
+        public virtual ICollection<Crate> Crates
+        {
+            get;
+            set;
+        }
+        public virtual ICollection<Group> Groups
+        {
+            get;
+            set;
+        }
     }
 }
