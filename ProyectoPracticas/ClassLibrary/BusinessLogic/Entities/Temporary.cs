daac62c35331ffcd1413c9308d7e4a5094161994
ismapolis <53060433+ismapolis@users.noreply.github.com>
2021-11-14 20:21:50 +0100

    todo sin errores, test aun peta

--- ProyectoPracticas/ClassLibrary/BusinessLogic/Entities/Temporary.cs
+++ ProyectoPracticas/ClassLibrary/BusinessLogic/Entities/Temporary.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TarongISW.Persistence.Entities
+namespace TarongISW.Entities
 {
     public partial class Temporary
     {
