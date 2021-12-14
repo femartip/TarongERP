e6bc5bfaf0e39ccbd14507bf2bdd2bc14103345b
Félix Martí Pérez <fmarper@upv.edu.es>
2021-12-14 17:34:48 +0100

    MAS GUI

---
+++ ProyectoPracticas/TarongISWGUI/Froms/NewContractForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TarongISWGUI
+{
+    public partial class TarongISWApp : Form
+    {
+        public TarongISWApp()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override bool fieldsOK()
+        {
+            int result;
+            return
+                base.fieldsOK() &&
+                !string.IsNullOrEmpty(textBoxDNI.Text) &&
+                !string.IsNullOrEmpty(textBoxBankAccount.Text) &&
+                !string.IsNullOrEmpty(textBoxHired.Text) &&
+                !string.IsNullOrEmpty(textBoxSNN.Text);
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (fieldsOk()) { }
+        }
+    }
+}
