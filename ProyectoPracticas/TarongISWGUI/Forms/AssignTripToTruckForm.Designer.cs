81222856c101e2987b105b5e95d43319a1647d24
David Sanfélix Enguídanos <dsaneng@upv.edu.es>
2021-12-18 20:40:33 +0100

    Asignar viaje a camión hecho

---
+++ ProyectoPracticas/TarongISWGUI/Forms/AssignTripToTruckForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace TarongISW.Presentation
+{
+    partial class AssignTripToTruckForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMatricula = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePickerSalida = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAñadir = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMatricula
+            // 
+            this.textBoxMatricula.Location = new System.Drawing.Point(130, 23);
+            this.textBoxMatricula.MaxLength = 7;
+            this.textBoxMatricula.Name = "textBoxMatricula";
+            this.textBoxMatricula.Size = new System.Drawing.Size(234, 22);
+            this.textBoxMatricula.TabIndex = 0;
+            this.textBoxMatricula.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.valorMatriculaModificado);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Matricula:";
+            // 
+            // dateTimePickerSalida
+            // 
+            this.dateTimePickerSalida.Enabled = false;
+            this.dateTimePickerSalida.Location = new System.Drawing.Point(130, 79);
+            this.dateTimePickerSalida.Name = "dateTimePickerSalida";
+            this.dateTimePickerSalida.Size = new System.Drawing.Size(234, 22);
+            this.dateTimePickerSalida.TabIndex = 2;
+            this.dateTimePickerSalida.ValueChanged += new System.EventHandler(this.fechaSalidaModificada);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fecha salida:";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Enabled = false;
+            this.dateTimePicker2.Location = new System.Drawing.Point(130, 130);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(234, 22);
+            this.dateTimePicker2.TabIndex = 4;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.fechaLlegadaModificada);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fecha llegada:";
+            // 
+            // buttonAñadir
+            // 
+            this.buttonAñadir.Enabled = false;
+            this.buttonAñadir.Location = new System.Drawing.Point(278, 182);
+            this.buttonAñadir.Name = "buttonAñadir";
+            this.buttonAñadir.Size = new System.Drawing.Size(75, 23);
+            this.buttonAñadir.TabIndex = 6;
+            this.buttonAñadir.Text = "Añadir";
+            this.buttonAñadir.UseVisualStyleBackColor = true;
+            this.buttonAñadir.Click += new System.EventHandler(this.añadirViajeACamión);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Volver";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.volverMenu);
+            // 
+            // AssignTripToTruckForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 219);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonAñadir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePickerSalida);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMatricula);
+            this.Name = "AssignTripToTruckForm";
+            this.Text = "SetTripToTruck";
+            this.Load += new System.EventHandler(this.AssignTripToTruckForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMatricula;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerSalida;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonAñadir;
+        private System.Windows.Forms.Button button2;
+    }
+}
